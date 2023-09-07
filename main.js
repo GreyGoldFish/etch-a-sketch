b2395885b4e7926cc0116ae31a391ceb0e37eda3
Lucas Aquino de Assis <GreyGoldFish@protonmail.com>
2023-09-07 04:48:29 -0300

    Add sketching through mouseover

--- main.js
+++ main.js
@@ -6,8 +6,14 @@ window.onload = main;
 function main() {
     const container = document.querySelector(".container");
 
+
     for (let index = 0; index < GRID_WIDTH * GRID_HEIGHT; index++) {
         const tile = document.createElement("div");
+        tile.addEventListener("mouseover", onMouseOver);;
         container.appendChild(tile);
     }
+}
+
+function onMouseOver() {
+    this.classList.toggle("mouseisover");
 }
