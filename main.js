72e6896a9159c31b9b052039ba68d649029542d7
Lucas Aquino de Assis <GreyGoldFish@protonmail.com>
2023-09-07 16:54:27 -0300

    Clamp grid size and clean up code

--- main.js
+++ main.js
@@ -1,36 +1,75 @@
+const min = 0
+const max = 100
+
+// Clamp number between two values with the following line:
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
 let mouseIsDown = false;
+let gridWidth = 16;
+let gridHeight = 16;
 
-window.onload = main;
+window.onload = createSketchPad;
 
 
-function main() {
-    const gridWidth = 16;
-    const gridHeight = 16;
-    const container = document.querySelector(".container");
+function createSketchPad() {
+    const sketchPad = document.querySelector(".sketch-pad-container");
+    const button = document.querySelector("button");
 
+    // Set grid size button
+    button.addEventListener("click", buttonClickedListener);
+    // Allows user to let go of the mouse button outside of the sketch pad
+    window.addEventListener("mouseup", windowMouseUpListener);
+
+    resetSketchPad(sketchPad);
 
     for (let index = 0; index < gridWidth * gridHeight; index++) {
         const tile = document.createElement("div");
-        tile.addEventListener("mousedown", mouseDownListener);
-        tile.addEventListener("mouseup", mouseUpListener);
-        tile.addEventListener("mouseover", mouseOverListener);
-        container.appendChild(tile);
+        tile.style.flexBasis = ((1 / gridWidth) * 100) + "%";
+        // Determine whether mouse is being dragged after mouse is down.
+        tile.addEventListener("mousedown", tileMouseDownListener);
+        tile.addEventListener("mouseover", tileMouseOverListener);
+        sketchPad.appendChild(tile);
+    }
+}
+
+function resetSketchPad(sketchPad) {
+    // Ensures that container has no children (resets)
+    while (sketchPad.lastElementChild) {
+        sketchPad.removeChild(sketchPad.lastElementChild);
     }
 }
 
 
-function mouseDownListener() {
+// Sets new grid according to prompt between 0 and 100.
+function buttonClickedListener() {
+    newWidth = prompt("Grid width:", 16);
+    newHeight = prompt("Grid height:", newWidth);
+
+    newWidth = clamp(newWidth, 0, 100);
+    newHeight = clamp(newHeight, 0, 100);
+
+    gridWidth = newWidth;
+    gridHeight = newHeight;
+
+    createSketchPad();
+}
+
+
+// Allows for painting the first tile clicked
+function tileMouseDownListener() {
     this.classList.toggle("painted");
     mouseIsDown = true;
 }
 
 
-function mouseUpListener() {
+// Stops painting
+function windowMouseUpListener() {
     mouseIsDown = false;
 }
 
 
-function mouseOverListener() {
+// Only paints if the user holds the mouse button down
+function tileMouseOverListener() {
     if (mouseIsDown) {
         this.classList.toggle("painted");
     }
