ff053948c4d6d1ba6c149dae10f75ae4ecd0239a
Lucas Aquino de Assis <GreyGoldFish@protonmail.com>
2023-09-08 13:28:46 -0300

    Add color picker and fix drawing

--- main.js
+++ main.js
@@ -3,18 +3,21 @@ const MAX_GRID_SIZE = 100
 
 let mouseIsDown = false;
 let gridSize = 16;
-let current_color = "rgb(0, 0, 0)";
+let currentColor = "black";
+let currentMode = "pencil";
 
 window.onload = createGrid;
 
 
 function createGrid() {
     const colorPicker = document.querySelector(".color-picker");
+    const eraserButton = document.querySelector(".eraser-button");
     const clearButton = document.querySelector(".clear-button");
     const gridSizeButton = document.querySelector(".grid-size-button");
     const grid = document.querySelector(".grid-container");
 
     colorPicker.addEventListener("input", colorPickerInputListener);
+    eraserButton.addEventListener("click", eraserButtonClickListener);
     clearButton.addEventListener("click", clearButtonClickListener);
     gridSizeButton.addEventListener("click", gridSizeButtonClickListener);
     // Allows user to let go of the mouse button outside of the sketch pad
@@ -40,16 +43,35 @@ function clearGrid() {
 }
 
 
-function clearButtonClickListener() {
-    clearGrid();
-    createGrid();
+function changeTile(tile) {
+    switch (currentMode) {
+        case "pencil":
+            tile.style.backgroundColor = currentColor;
+            break;
+        case "erase":
+            tile.style.backgroundColor = "";
+            break;
+        default:
+            break;
+    }
 }
 
 
 function colorPickerInputListener(event) {
-    current_color = event.target.value;
+    currentMode = "pencil";
+    currentColor = event.target.value;
 }
 
+
+function eraserButtonClickListener() {
+    currentMode = "erase";
+}
+
+
+function clearButtonClickListener() {
+    clearGrid();
+    createGrid();
+}
 
 // Sets new grid according to prompt between 0 and 100.
 function gridSizeButtonClickListener() {
@@ -63,26 +85,22 @@ function gridSizeButtonClickListener() {
 }
 
 
-// Allows for painting the first tile clicked
-function tileMouseDownListener() {
+// Allows for drawing the first tile clicked
+function tileMouseDownListener(event) {
     mouseIsDown = true;
+    changeTile(event.target)
 }
 
 
-// Stops painting if mouse is down
+// Stops drawing if mouse is down
 function windowMouseUpListener() {
     mouseIsDown = false;
 }
 
 
-function paintTile(tile) {
-    tile.style.backgroundColor = current_color;
-}
-
-
-// Only paints if the user holds the mouse button down and is over a tile
+// Only draws if the user holds the mouse button down and is over a tile
 function tileMouseOverListener(event) {
     if (mouseIsDown) {
-        paintTile(event.target);
+        changeTile(event.target)
     }
 }
